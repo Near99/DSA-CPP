71e269d2e5e304da61492b280573bab6a5f569e2
Charles <wkx0823@gmail.com>
2022-10-25 15:12:34 +0800

    Add files via upload

---
+++ array/two_number_sum.cpp
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+vector<int> two_number_sum(vector<int> array, int target)
+{
+    unordered_set<int> set;
+    for (int num : array) {
+        int i = target - num;
+        if (set.find(i) != set.end()) {
+            return vector<int> { i, num };
+        }
+        set.insert(num);
+    }
+    return {};
+}
+
+int main()
+{
+    vector<int> input = { 3, 5, -4, 8, 11, 1, -1, 6 };
+    vector<int> aws = { 11, -1 };
+    vector<int> res = two_number_sum(input, 10);
+    for (int i = 0; i < res.size(); i++) {
+        assert(res[i] == aws[i]);
+    }
+}

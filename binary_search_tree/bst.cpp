71327f9971c4d5dac790ac971a6a2abd9df974cd
Charles <wkx0823@gmail.com>
2022-10-25 20:22:50 +0800

    Add files via upload

---
+++ binary_search_tree/bst.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int value;
+    struct Node* left;
+    struct Node* right;
+};
+
+class BinarySearchTree {
+
+public:
+    BinarySearchTree()
+    {
+        _root = nullptr;
+    }
+
+    void insert(int value)
+    {
+        _insert(&_root, value);
+    }
+
+    void traverse()
+    {
+        _traverse(_root);
+    }
+
+    void print()
+    {
+        cout << _root->value << endl;
+    }
+
+private:
+    struct Node* _root;
+
+    void _insert(struct Node** root, int value)
+    {
+        if ((*root) == nullptr) {
+            cout << "called" << endl;
+            struct Node* new_node = new struct Node;
+            new_node->value = value;
+            new_node->left = nullptr;
+            new_node->right = nullptr;
+            *root = new_node;
+        } else if (value < (*root)->left->value) {
+            _insert(&(*root)->left, value);
+        } else {
+            _insert(&(*root)->right, value);
+        }
+    }
+
+    void _traverse(struct Node* root)
+    {
+        if (root == nullptr)
+            return;
+        _traverse(root->left);
+        cout << root->value << endl;
+        _traverse(root->right);
+    }
+};
+
+int main()
+{
+    BinarySearchTree bst;
+    bst.insert(50);
+    bst.insert(20);
+    bst.insert(100);
+    bst.insert(24);
+    bst.insert(15);
+    bst.insert(30);
+    // bst.traverse();
+    // bst.print();
+    return 0;
+}

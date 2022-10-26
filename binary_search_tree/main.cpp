3906ad674c0865048a4ebcfeb59135bdbccd6356
Charles <wkx0823@gmail.com>
2022-10-26 20:35:50 +0800

    Update main.cpp

--- binary_search_tree/main.cpp
+++ binary_search_tree/main.cpp
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include <vector>
 
 #include "bst.h"
@@ -7,7 +9,9 @@ using namespace std;
 
 int main() {
     BinarySearchTree tree;
-    vector<int> v = {10, 5, 15, 2, 9, 13, 22, 1, 14};
+    vector<int> v = {10, 5, 15, 2, 3, 4, 9, 13, 22, 1, 14};
+    vector<int> p = {120, 105, 125, 12, 99, 131, 222, 1872, 140};
+    assert(tree.isEmpty() == true);
     for (int n : v) {
         tree.insert(n);
     }
@@ -16,5 +20,17 @@ int main() {
     cout << "Min: " << tree.getMin() << endl;
     cout << "Height: " << tree.getHeight() << endl;
     cout << "Valid? : " << tree.isValid() << endl;
+    for (int n : v) {
+        assert(tree.contain(n) == true);
+    }
+    for (int n : p) {
+        assert(tree.contain(n) == false);
+    }
+    for (int n : v) {
+        tree.remove(n);
+        assert(tree.isValid() == true);
+    }
+    assert(tree.isEmpty() == true);
+    assert(tree.isValid() == true);
     return 0;
 }

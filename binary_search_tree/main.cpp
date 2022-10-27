ffd7261fe2074245e1551d94098d3d6839cb2d54
Charles <wkx0823@gmail.com>
2022-10-27 17:28:07 +0800

    Update main.cpp

--- binary_search_tree/main.cpp
+++ binary_search_tree/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
     BinarySearchTree tree;
-    vector<int> v = {10, 5, 15, 2, 3, 4, 9, 13, 22, 1, 14};
+    vector<int> v = {10, 5, 15, 2, 3, 4, 9, 13, 22, 1, 14, 19};
     vector<int> p = {120, 105, 125, 12, 99, 131, 222, 1872, 140};
     assert(tree.isEmpty() == true);
     for (int n : v) {
@@ -26,11 +26,16 @@ int main() {
     for (int n : p) {
         assert(tree.contain(n) == false);
     }
+    assert(tree.successor(4) == 5);
+    assert(tree.successor(1) == 2);
+    assert(tree.successor(14) == 15);
+    assert(tree.successor(9) == 10);
     for (int n : v) {
         tree.remove(n);
         assert(tree.isValid() == true);
     }
     assert(tree.isEmpty() == true);
     assert(tree.isValid() == true);
+
     return 0;
 }

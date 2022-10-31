e64ac8ebdf6e0989ca14828a5e48f02fce3724dd
Charles <wkx0823@gmail.com>
2022-10-31 16:35:21 +0800

    Update main.cpp

--- priority_queue/main.cpp
+++ priority_queue/main.cpp
@@ -1,9 +1,22 @@
+#include "assert.h"
 #include "pq.h"
 
 using namespace pq;
 
 int main() {
-    PriorityQueue q(20);
-    q.size();
+    const int Q_CAP = 10;
+    PriorityQueue q(Q_CAP);
+    assert(q.size() == 0 && q.capacity() == Q_CAP);
+    assert(q.empty() == true && q.full() == false);
+    int intput_arr[] = {66, 3, 899, 24, 123, 758, 97, 77, 222, 1};
+    for (int i = 0; i < 10; i++) {
+        q.insert(intput_arr[i]);
+    }
+    assert(q.empty() == false && q.full() == true);
+    for (int i = 0; i < 2; i++) {
+        q.extractMax();
+    }
+    q.print();
+
     return 0;
 }

66acd715a5f5b3799412cabe33f8a470a4ed0d3b
Charles <wkx0823@gmail.com>
2022-11-16 17:14:46 +0800

    Add files via upload

---
+++ graphs/main.cpp
@@ -0,0 +1,28 @@
+#include "adjacency_matrices.h"
+
+int main() {
+    gam::Graph g(8);
+    g.addEdge(0, 1);
+    g.addEdge(1, 0);
+    g.addEdge(1, 2);
+    g.addEdge(2, 1);
+    g.addEdge(2, 3);
+    g.addEdge(3, 2);
+    g.addEdge(3, 4);
+    g.addEdge(3, 5);
+    g.addEdge(4, 3);
+    g.addEdge(4, 5);
+    g.addEdge(4, 6);
+    g.addEdge(5, 3);
+    g.addEdge(5, 4);
+    g.addEdge(5, 6);
+    g.addEdge(5, 7);
+    g.addEdge(6, 4);
+    g.addEdge(6, 5);
+    g.addEdge(6, 7);
+    g.addEdge(7, 6);
+    g.addEdge(7, 5);
+    g.print();
+    g.bfs(4);
+    return 0;
+}
